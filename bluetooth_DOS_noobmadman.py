fd239da76f589fd260581f27a14a6bf033a5398e
noobmadman <150483877+noobmadman@users.noreply.github.com>
2024-05-13 22:39:06 +0300

    bluetooth_DOS_noobmadman.py

--- bluetooth_DOS_noobmadman.py
+++ bluetooth_DOS_noobmadman.py
@@ -3,14 +3,14 @@ import subprocess
 
 def scan_devices():
     try:
-        print("Scanning...")
+        print("Scanning...wait.")
         output = subprocess.check_output("hcitool scan", shell=True, stderr=subprocess.STDOUT, text=True)
         lines = output.splitlines()
         devices = {}
-        for line in lines[1:]:  # Skip the header line
+        for line in lines[1:]: 
             info = line.split()
             mac = info[0]
-            device_name = ' '.join(info[2:])  # Join device name if it contains spaces
+            device_name = ' '.join(info[2:])  
             devices[mac] = device_name
         return devices
     except subprocess.CalledProcessError as e:
@@ -19,13 +19,13 @@ def scan_devices():
 
 def print_device_info(devices):
     if devices:
-        print("| id |   MAC Address  |   Device Name   |")
+        print("/_id/______/MAC_Address/____/Device_Name__/")
         print("---------------------------------------")
         for idx, (mac, name) in enumerate(devices.items()):
             print(f"| {idx}  |   {mac}  |   {name}")
         print("---------------------------------------")
     else:
-        print("No devices found.")
+        print("No devices found idk maybe try again.")
 
 def main():
     print("""    _   _  _ _____ ___   __  __   _   _  _ ___ _    ___ 
@@ -33,11 +33,11 @@ def main():
   / _ \| .` | | |  | |  | |\/| |/ _ \| .` | _|| |__| _| 
  /_/ \_\_|\_| |_| |___|_|_|  |_/_/ \_\_|\_|___|____|___|
                      |___|""")
-    print("This is a simple Python script to scan for Bluetooth devices and ping them.")
+    print("This is a simple Python script to scan for Bluetooth devices and DOS them with l2ping tool.")
     print("Made by noobmadman")
     print("https://github.com/noobmadman")
     
-    accept = input("Do you want to proceed? (y/n): ").lower()
+    accept = input("Do you want to do this? (y/n): ").lower()
     if accept != "y":
         print("Exiting...")
         return
@@ -56,7 +56,7 @@ def main():
         else:
             print("Invalid ID or MAC address.")
     else:
-        print("No devices found.")
+        print("No devices found idk maybe try again.")
 
 if __name__ == "__main__":
     main()
